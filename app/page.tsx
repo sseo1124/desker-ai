e74019aa73164feafc918e07a910be2bbd230308
김태헌 <7w7w7w7w@naver.com>
2025-08-29 00:21:46 +0900

    design: 랜딩페이지에 홍보문구 추가

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,12 @@ const LandingPage = () => {
           height={150}
           alt="Desker Logo"
         />
+        <div className="flex justify-center items-center -mt-2 md:-mt-3 lg:-mt-4">
+          <span className="block font-bold text-4xl text-center">
+            고객과 대화하는 데스커 AI 로<br/>
+            떠나는 고객을 붙잡아 매출로 전환하세요
+          </span>
+        </div>
         <div className="flex justify-center items-center rounded-lg bg-brown-400 p-0.5 ">
           <Image
             src="/information-section-image.png"
