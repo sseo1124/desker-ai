eedeccc49388ba6542c11f9bd68e381dbf638fdc
Sanghyuk Seo <sseo951124@gmail.com>
2025-08-28 15:20:12 +0900

    feat: 보일러 플레이트

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import '@/app/ui/global.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
