6261e567c9bb9cd0c47e48ca518aded30f3da476
김태헌 <7w7w7w7w@naver.com>
2025-09-14 08:50:26 +0900

    design: 사이드바 툴팁 구현

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -9,6 +9,13 @@ import {
   SidebarMenu,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
+
 import { MessageCircleMore, ContactRound, Bot, UserCog } from "lucide-react";
 import { SIDE_BAR_TOOLTIP_MESSAGE } from "@/config/constants";
 
@@ -58,9 +65,16 @@ const Layout = async () => {
                     key={item.title}
                     className="flex justify-center"
                   >
-                    <Link href={item.url}>
-                      <item.icon className="h-8 w-8 text-sidebar-foreground" />
-                    </Link>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Link href={item.url}>
+                          <item.icon className="h-8 w-8 text-sidebar-foreground" />
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent side="right" sideOffset={10}>
+                        <p className="text-sm">{item.text}</p>
+                      </TooltipContent>
+                    </Tooltip>
                   </SidebarMenuItem>
                 ))}
               </SidebarMenu>
